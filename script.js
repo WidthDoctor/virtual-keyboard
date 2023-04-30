830c023465c309c1c15df6a7aded69dc24d1bc80
WidthDoctor <elskel1812@gmail.com>
2023-05-01 00:15:34 +0200

    feat: add workable capslock key (Пн, 01 май 2023 00:15:34)

--- script.js
+++ script.js
@@ -1,44 +1,292 @@
 const ru = [
-    "ё","1", "2", "3", "4", "5", "6", "7", "8", "9", "0","-", "=","backspace",
-    "Tab","й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з","х","ъ","\\","del",
-    "caps", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж","э","enter",
-    "shift","я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".","up","shift",
-    "ctrl","alt","space","alt","ctrl","left","down","right"
+  "ё",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  "-",
+  "=",
+  "Backspace",
+  "Tab",
+  "й",
+  "ц",
+  "у",
+  "к",
+  "е",
+  "н",
+  "г",
+  "ш",
+  "щ",
+  "з",
+  "х",
+  "ъ",
+  "\\",
+  "del",
+  "CapsLock",
+  "ф",
+  "ы",
+  "в",
+  "а",
+  "п",
+  "р",
+  "о",
+  "л",
+  "д",
+  "ж",
+  "э",
+  "enter",
+  "Shift",
+  "я",
+  "ч",
+  "с",
+  "м",
+  "и",
+  "т",
+  "ь",
+  "б",
+  "ю",
+  ".",
+  "up",
+  "Shift",
+  "ctrl",
+  "alt",
+  "space",
+  "alt",
+  "ctrl",
+  "left",
+  "down",
+  "right",
 ];
 const ruShift = [
-  "Ё","!", "\"", "№", ";", "%", ":", "?", "*", "(", ")","_", "+","backspace",
-  "Tab","Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З","Х","Ъ","\/","del",
-  "caps", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж","Э","enter",
-  "shift","Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", ",","up","shift",
-  "ctrl","alt","space","alt","ctrl","left","down","right"
+  "Ё",
+  "!",
+  '"',
+  "№",
+  ";",
+  "%",
+  ":",
+  "?",
+  "*",
+  "(",
+  ")",
+  "_",
+  "+",
+  "Backspace",
+  "Tab",
+  "Й",
+  "Ц",
+  "У",
+  "К",
+  "Е",
+  "Н",
+  "Г",
+  "Ш",
+  "Щ",
+  "З",
+  "Х",
+  "Ъ",
+  "/",
+  "del",
+  "CapsLock",
+  "Ф",
+  "Ы",
+  "В",
+  "А",
+  "П",
+  "Р",
+  "О",
+  "Л",
+  "Д",
+  "Ж",
+  "Э",
+  "enter",
+  "Shift",
+  "Я",
+  "Ч",
+  "С",
+  "М",
+  "И",
+  "Т",
+  "Ь",
+  "Б",
+  "Ю",
+  ",",
+  "up",
+  "Shift",
+  "ctrl",
+  "alt",
+  "space",
+  "alt",
+  "ctrl",
+  "left",
+  "down",
+  "right",
 ];
 
 const en = [
-    "`","1", "2", "3", "4", "5", "6", "7", "8", "9", "0","-", "=","backspace",
-    "Tab","q", "w", "e", "r", "t", "y", "u", "i", "o", "p","[","]","\\","del",
-    "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";","'","enter",
-    "shift","z", "x", "c", "v", "b", "n", "m", ",", ".", "/","up","shift",
-    "ctrl","alt","space","alt","ctrl","left","down","right"
+  "`",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  "-",
+  "=",
+  "Backspace",
+  "Tab",
+  "q",
+  "w",
+  "e",
+  "r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "[",
+  "]",
+  "\\",
+  "del",
+  "CapsLock",
+  "a",
+  "s",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  ";",
+  "'",
+  "enter",
+  "Shift",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+  "n",
+  "m",
+  ",",
+  ".",
+  "/",
+  "up",
+  "Shift",
+  "ctrl",
+  "alt",
+  "space",
+  "alt",
+  "ctrl",
+  "left",
+  "down",
+  "right",
 ];
 const enShift = [
-  "~","!", "@", "#", "$", "%", "^", "&", "*", "(", ")","_", "+","backspace",
-  "Tab","Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P","{","}","|","del",
-  "caps", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":","\"","enter",
-  "shift","Z", "X", "C", "V", "B", "N", "M", "<", ">", "?","up","shift",
-  "ctrl","alt","space","alt","ctrl","left","down","right"
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "+",
+  "Backspace",
+  "Tab",
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "{",
+  "}",
+  "|",
+  "del",
+  "CapsLock",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  ":",
+  '"',
+  "enter",
+  "Shift",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "<",
+  ">",
+  "?",
+  "up",
+  "Shift",
+  "ctrl",
+  "alt",
+  "space",
+  "alt",
+  "ctrl",
+  "left",
+  "down",
+  "right",
 ];
+const upperEN = en.map(function (str) {
+  if (str.length === 1) {
+    return str.toUpperCase();
+  } else {
+    return str;
+  }
+});
+const upperRU = ru.map(function (str) {
+  if (str.length === 1) {
+    return str.toUpperCase();
+  } else {
+    return str;
+  }
+});
+console.log(en[12].length);
 let currentLang = en;
 let shiftPressed = false;
+let capsPressed = false;
 
-const input = document.createElement('textarea');
-input.classList.add('input');
+const input = document.createElement("textarea");
+input.classList.add("input");
 document.body.append(input);
 
-const keyboardBox = document.createElement('div');
-keyboardBox.classList.add('keyboard-container');
+const keyboardBox = document.createElement("div");
+keyboardBox.classList.add("keyboard-container");
 document.body.append(keyboardBox);
 
-function createKeyboard(){
+function createKeyboard() {
   currentLang.forEach((key, i) => {
     const button = document.createElement("button");
     button.classList.add("key");
@@ -49,9 +297,9 @@ function createKeyboard(){
     }
     if (
       key === "enter" ||
-      key === "caps" ||
-      key === "backspace" ||
-      key === "shift"
+      key === "CapsLock" ||
+      key === "Backspace" ||
+      key === "Shift"
     ) {
       wideKey(button);
     }
@@ -63,18 +311,16 @@ function createKeyboard(){
 }
 createKeyboard();
 function wideKey(button) {
-    button.classList.add("wide-key");
+  button.classList.add("wide-key");
 }
 
 document.body.appendChild(keyboardBox);
-
-
 
 function writeMashine(event) {
   const target = event.target;
   input.focus();
   // проверяем наличие кнопок на странице
-  const existingKeys = document.querySelectorAll('.key');
+  const existingKeys = document.querySelectorAll(".key");
   if (existingKeys.length > 0) {
     // если кнопки есть, то заменяем на новый язык
     replaceKeys(currentLang);
@@ -88,10 +334,10 @@ function writeMashine(event) {
     if (target.textContent === "Tab") {
       event.preventDefault();
       handleTabKeyPress(event);
-    }else if (target.textContent === "shift"){
+    } else if (target.textContent === "Shift") {
       event.preventDefault();
       handleShiftKeyPress();
-    }else if (target.textContent === "backspace") {
+    } else if (target.textContent === "Backspace") {
       event.preventDefault();
       handleBackspaceKeyPress(event);
     } else if (target.textContent === "space") {
@@ -100,7 +346,11 @@ function writeMashine(event) {
     } else if (target.textContent === "del") {
       event.preventDefault();
       handleDelKeyPress();
-    } else{
+    } else if (target.textContent === "CapsLock") {
+      capsPressed = true;
+      console.log(capsPressed);
+      caps();
+    } else {
       input.value =
         input.value.slice(0, start) + character + input.value.slice(end);
       input.setSelectionRange(start + 1, start + 1);
@@ -108,125 +358,145 @@ function writeMashine(event) {
   }
 }
 
-  const keyboardKeys = document.querySelectorAll(".key");
+const keyboardKeys = document.querySelectorAll(".key");
 
-  document.addEventListener("keydown", (event) => {
-    const key = event.key.toLowerCase();
-    keyboardKeys.forEach((keyButton) => {
-      if (event.ctrlKey && event.altKey) {
-        if (currentLang==en) {//работа шифта
-          currentLang=ru;
-          replaceKeys(ru);
-        }else if (currentLang==ru) {
-          currentLang=en;
-          replaceKeys(en);
-        }else if(currentLang==enShift){
-          currentLang=ruShift;
-          replaceKeys(ruShift);
-        }else if (currentLang==ruShift) {
-          currentLang=enShift;
-          replaceKeys(enShift);
-        }
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+  console.log(key);
+  keyboardKeys.forEach((keyButton) => {
+    if (event.ctrlKey && event.altKey) {
+      if (currentLang == en) {
+        //работа шифта
+        currentLang = ru;
+        replaceKeys(ru);
+      } else if (currentLang == ru) {
+        currentLang = en;
+        replaceKeys(en);
+      } else if (currentLang == enShift) {
+        currentLang = ruShift;
+        replaceKeys(ruShift);
+      } else if (currentLang == ruShift) {
+        currentLang = enShift;
+        replaceKeys(enShift);
       }
-      if (event.key === "Shift") {
-        shiftPressed = true;
-        handleShiftKeyPress();
-      }
-      if (keyButton.textContent.toLowerCase() === key) {
-        keyButton.classList.add("active");
-        input.focus();
-      }
-    });
-    if (event.key === "Backspace") {
-      event.preventDefault();
-      handleBackspaceKeyPress(event);
     }
-    if (event.key === "Tab") {
-      event.preventDefault();
-      handleTabKeyPress(event);
+    if (event.key === "Shift") {
+      shiftPressed = true;
+      handleShiftKeyPress();
+    }
+    if (keyButton.textContent === key) {
+      keyButton.classList.add("active");
+      input.focus();
     }
   });
 
-  document.addEventListener("keyup", (event) => {
-    const key = event.key.toLowerCase();
-    keyboardKeys.forEach((keyButton) => {
-      if (event.key === "Shift") {
-        shiftPressed = false;
-        handleShiftKeyPress();
-      }
-      if (keyButton.textContent.toLowerCase() === key) {
-        keyButton.classList.remove("active");
-      }
-    });
-  });
-
-  function handleSpaceKeyPress(){
-    const cursorPosition = input.selectionStart;
-    const textBeforeCursor = input.value.slice(0, cursorPosition);
-    const textAfterCursor = input.value.slice(cursorPosition);
-    const newText = textBeforeCursor + " " + textAfterCursor;
-    input.value = newText;
-    input.setSelectionRange(cursorPosition + 1, cursorPosition + 1);
-  }
-  function handleDelKeyPress() {
-    const cursorPosition = input.selectionEnd;
-
-    input.value = input.value.slice(0, cursorPosition) + input.value.slice(cursorPosition + 1);
-    input.setSelectionRange(cursorPosition, cursorPosition);
-  }
-
-  function handleShiftKeyPress() {
-    if(currentLang==en){
-      currentLang = enShift;
-      replaceKeys(enShift);
-    }else if(currentLang==enShift){
-      currentLang = en;
-      replaceKeys(en);
-    }else if (currentLang==ru) {
-      currentLang=ruShift;
-      replaceKeys(ruShift);
-    }else if (currentLang==ruShift) {
-      currentLang=ru;
-      replaceKeys(ru);
-    }
-  }
-  function replaceKeys(lang) {
-    const keys = document.querySelectorAll('.key');
-    keys.forEach((key, i) => {
-      key.textContent = lang[i];
-    });
-  }
-  function handleTabKeyPress(event) {
+  if (event.key === "CapsLock") {
+    capsPressed = true;
+    console.log(capsPressed);
     event.preventDefault();
-    const cursorPosition = input.selectionStart;
-    const textBeforeCursor = input.value.slice(0, cursorPosition);
-    const textAfterCursor = input.value.slice(cursorPosition);
-    const newText = textBeforeCursor + "    " + textAfterCursor;
-    input.value = newText;
-    input.setSelectionRange(cursorPosition + 4, cursorPosition + 4);
+    caps();
   }
-
-
-  function handleBackspaceKeyPress(event) {
-    const start = input.selectionStart;
-    const end = input.selectionEnd;
-    // если выделен текст, то удаляем его полностью
-    if (start !== end) {
-      input.value = input.value.slice(0, start) + input.value.slice(end);
-      input.setSelectionRange(start, start);
-      return;
-    }
-    // если перед курсором есть 4 пробела табуляции, то удаляем их
-    if (input.value.slice(start - 4, start) === "    ") {
-      input.value = input.value.slice(0, start - 4) + input.value.slice(start);
-      input.setSelectionRange(start - 4, start - 4);
-      return;
-    }
-    // иначе удаляем один символ перед курсором
-    input.value = input.value.slice(0, start - 1) + input.value.slice(start);
-    input.setSelectionRange(start - 1, start - 1);
+  if (event.key === "Backspace") {
+    event.preventDefault();
+    handleBackspaceKeyPress(event);
   }
+  if (event.key === "Tab") {
+    event.preventDefault();
+    handleTabKeyPress(event);
+  }
+});
 
+document.addEventListener("keyup", (event) => {
+  const key = event.key;
+  keyboardKeys.forEach((keyButton) => {
+    if (event.key === "Shift") {
+      shiftPressed = false;
+      handleShiftKeyPress();
+    }
+    if (keyButton.textContent === key) {
+      keyButton.classList.remove("active");
+    }
+  });
+});
 
-    keyboardBox.addEventListener("click", writeMashine);
-    keyboardBox.addEventListener("keydown", writeMashine);
+function handleSpaceKeyPress() {
+  const cursorPosition = input.selectionStart;
+  const textBeforeCursor = input.value.slice(0, cursorPosition);
+  const textAfterCursor = input.value.slice(cursorPosition);
+  const newText = textBeforeCursor + " " + textAfterCursor;
+  input.value = newText;
+  input.setSelectionRange(cursorPosition + 1, cursorPosition + 1);
+}
+function handleDelKeyPress() {
+  const cursorPosition = input.selectionEnd;
+  input.value =
+    input.value.slice(0, cursorPosition) +
+    input.value.slice(cursorPosition + 1);
+  input.setSelectionRange(cursorPosition, cursorPosition);
+}
+function caps() {
+  if (capsPressed === true && currentLang == en) {
+    capsPressed = true;
+    currentLang = upperEN;
+    replaceKeys(upperEN);
+  } else if (currentLang === upperEN) {
+    capsPressed = false;
+    currentLang = en;
+    replaceKeys(en);
+  }
+  if (currentLang == ru) {
+    capsPressed = true;
+    currentLang = upperRU;
+    replaceKeys(upperRU);
+  } else if (currentLang === upperRU) {
+    capsPressed = false;
+    currentLang = ru;
+    replaceKeys(ru);
+  }
+}
+
+function replaceKeys(lang) {
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((key, i) => {
+    key.textContent = lang[i];
+  });
+}
+function handleShiftKeyPress() {
+  currentLang === en ? (currentLang = enShift, replaceKeys(enShift)) :
+  currentLang === enShift ? (currentLang = en, replaceKeys(en)) :
+  currentLang === ru ? (currentLang = ruShift, replaceKeys(ruShift)) :
+  (currentLang = ru, replaceKeys(ru));
+}
+function handleTabKeyPress(event) {
+  event.preventDefault();
+  const cursorPosition = input.selectionStart;
+  const textBeforeCursor = input.value.slice(0, cursorPosition);
+  const textAfterCursor = input.value.slice(cursorPosition);
+  const newText = textBeforeCursor + "    " + textAfterCursor;
+  input.value = newText;
+  input.setSelectionRange(cursorPosition + 4, cursorPosition + 4);
+}
+
+function handleBackspaceKeyPress(event) {
+  event.preventDefault();
+  const start = input.selectionStart;
+  const end = input.selectionEnd;
+  // если выделен текст, то удаляем его полностью
+  if (start !== end) {
+    input.value = input.value.slice(0, start) + input.value.slice(end);
+    input.setSelectionRange(start, start);
+    return;
+  }
+  // если перед курсором есть 4 пробела табуляции, то удаляем их
+  if (input.value.slice(start - 4, start) === "    ") {
+    input.value = input.value.slice(0, start - 4) + input.value.slice(start);
+    input.setSelectionRange(start - 4, start - 4);
+    return;
+  }
+  // иначе удаляем один символ перед курсором
+  input.value = input.value.slice(0, start - 1) + input.value.slice(start);
+  input.setSelectionRange(start - 1, start - 1);
+}
+
+keyboardBox.addEventListener("click", writeMashine);
+keyboardBox.addEventListener("keydown", writeMashine);
