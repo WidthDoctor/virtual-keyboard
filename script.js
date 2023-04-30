144f574e02343e4bfdf003a47dc29b0022297302
WidthDoctor <elskel1812@gmail.com>
2023-04-30 08:02:59 +0200

    feat: add functional tab and backspace keys (Вс, 30 апр 2023 08:02:59)

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const en = [
     "ctrl","alt","space","alt","ctrl","left","down","right"
 ];
 let currentLang = en;
-console.log(currentLang.indexOf('ctrl'));
+
 const input = document.createElement('textarea');
 input.classList.add('input');
 document.body.append(input);
@@ -22,7 +22,6 @@ document.body.append(input);
 const keyboardBox = document.createElement('div');
 keyboardBox.classList.add('keyboard-container');
 document.body.append(keyboardBox);
-
 
 currentLang.forEach((key, i) => {
   const button = document.createElement("button");
@@ -53,37 +52,88 @@ document.body.appendChild(keyboardBox);
 
 
 
-function writeMachine(event) {
-    const target = event.target;
-    if (target.classList.contains("key")) {
-      const character = target.textContent;
-      const start = input.selectionStart;
-      const end = input.selectionEnd;
-      input.value = input.value.slice(0, start) + character + input.value.slice(end);
+function writeMashine(event) {
+  const target = event.target;
+  input.focus();
+  //шукаю клик
+  if (target.classList.contains("key")) {
+    const start = input.selectionStart;
+    const character = target.textContent;
+    const end = input.selectionEnd;
+    if (target.textContent === "Tab") {
+      event.preventDefault();
+      handleTabKeyPress(event);
+    } else if (target.textContent === "backspace") {
+      event.preventDefault();
+      handleBackspaceKeyPress(event);
+    } else {
+      input.value =
+        input.value.slice(0, start) + character + input.value.slice(end);
       input.setSelectionRange(start + 1, start + 1);
-      input.focus();
     }
   }
+}
 
-  keyboardBox.addEventListener("click", writeMachine);
+  const keyboardKeys = document.querySelectorAll(".key");
 
-
-  const keyboardKeys = document.querySelectorAll(".key"); // тут на нажатиях синхрон клавиш
-
-document.addEventListener("keydown", (event) => {
-  const key = event.key.toLowerCase();
-  keyboardKeys.forEach((keyButton) => {
-    if (keyButton.textContent.toLowerCase() === key) {
-      keyButton.classList.add("active");
-    }
-});
-});
-
-document.addEventListener("keyup", (event) => {
-  const key = event.key.toLowerCase();
-  keyboardKeys.forEach((keyButton) => {
-    if (keyButton.textContent.toLowerCase() === key) {
-      keyButton.classList.remove("active");
+  document.addEventListener("keydown", (event) => {
+    const key = event.key.toLowerCase();
+    keyboardKeys.forEach((keyButton) => {
+      if (keyButton.textContent.toLowerCase() === key) {
+        keyButton.classList.add("active");
+        input.focus();
+      }
+    });
+    if (event.key === "Backspace") { //доделать при нажатии на виртуалке чтобы все чотко работало
+          event.preventDefault();
+          handleBackspaceKeyPress(event);
+        }
+    if (event.key === "Tab") {
+      event.preventDefault();
+      handleTabKeyPress(event);
     }
   });
-});
+
+  document.addEventListener("keyup", (event) => {
+    const key = event.key.toLowerCase();
+    keyboardKeys.forEach((keyButton) => {
+      if (keyButton.textContent.toLowerCase() === key) {
+        keyButton.classList.remove("active");
+      }
+    });
+  });
+
+  function handleTabKeyPress(event) {
+    event.preventDefault();
+    const cursorPosition = input.selectionStart;
+    const textBeforeCursor = input.value.slice(0, cursorPosition);
+    const textAfterCursor = input.value.slice(cursorPosition);
+    const newText = textBeforeCursor + "    " + textAfterCursor;
+    input.value = newText;
+    input.setSelectionRange(cursorPosition + 4, cursorPosition + 4);
+  }
+
+
+  function handleBackspaceKeyPress(event) {
+    const start = input.selectionStart;
+    const end = input.selectionEnd;
+    // если выделен текст, то удаляем его полностью
+    if (start !== end) {
+      input.value = input.value.slice(0, start) + input.value.slice(end);
+      input.setSelectionRange(start, start);
+      return;
+    }
+    // если перед курсором есть 4 пробела табуляции, то удаляем их
+    if (input.value.slice(start - 4, start) === "    ") {
+      input.value = input.value.slice(0, start - 4) + input.value.slice(start);
+      input.setSelectionRange(start - 4, start - 4);
+      return;
+    }
+    // иначе удаляем один символ перед курсором
+    input.value = input.value.slice(0, start - 1) + input.value.slice(start);
+    input.setSelectionRange(start - 1, start - 1);
+  }
+
+
+    keyboardBox.addEventListener("click", writeMashine);
+    keyboardBox.addEventListener("keydown", writeMashine);
